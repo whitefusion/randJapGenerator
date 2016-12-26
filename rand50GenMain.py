33ea3efd0e38ab65c2bfd111ffa242e5beb2e2f9
ShinBai <whitefusion@163.com>
2016-12-26 09:48:40 -0500

    Generate Table succeed

---
+++ rand50GenMain.py
@@ -0,0 +1,28 @@
+from skimage import io
+from skimage.viewer import ImageViewer
+from skimage.draw import polygon_perimeter
+from drawTable import drawTable
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import skimage
+
+# import images
+sheet_empty = io.imread("material\empty.JPG")
+sheet_answer = io.imread("material\sampleSol.JPG")
+
+print sheet_answer.shape
+print sheet_empty.shape
+viewer = ImageViewer(sheet_answer)
+#viewer.show()
+
+# initialize parameter
+pageS = (1018,720)
+cellS = (35,40)
+rows = 23
+cols = 15
+
+# draw table
+page = drawTable(pageS,cellS,rows,cols)
+plt.imshow(page,cmap = 'gray')
+plt.show()
