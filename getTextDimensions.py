b755d3b225754b85c82af03ce6f3d22f2a834dc4
ShinBai <whitefusion@163.com>
2017-01-08 07:51:14 -0500

    This function can get the pixel length of a string.

---
+++ getTextDimensions.py
@@ -0,0 +1,20 @@
+import ctypes
+
+def GetTextDimensions(text, points, font):
+    class SIZE(ctypes.Structure):
+        _fields_ = [("cx", ctypes.c_long), ("cy", ctypes.c_long)]
+
+    hdc = ctypes.windll.user32.GetDC(0)
+    hfont = ctypes.windll.gdi32.CreateFontA(points, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, font)
+    hfont_old = ctypes.windll.gdi32.SelectObject(hdc, hfont)
+
+    size = SIZE(0, 0)
+    ctypes.windll.gdi32.GetTextExtentPoint32A(hdc, text, len(text), ctypes.byref(size))
+
+    ctypes.windll.gdi32.SelectObject(hdc, hfont_old)
+    ctypes.windll.gdi32.DeleteObject(hfont)
+
+    return (size.cx, size.cy)
+
+#print(GetTextDimensions("Hello world", 12, "Times New Roman"))
+#print(GetTextDimensions("Hello world", 12, "Arial"))
